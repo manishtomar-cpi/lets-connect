69a770efb89d053e0fd70a0c2133b05f44157ae7
Manish Tomar <Manish.Tomar@uk-cpi.com>
2024-08-20 14:29:11 +0100

    updated logout

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -1,14 +1,13 @@
 import { serialize } from 'cookie';
 
 export default function handler(req, res) {
-  console.log('in logout >>>>>>>>>>.hit')
   if (req.method === 'POST') {
     res.setHeader('Set-Cookie', serialize('token', '', {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',  // Secure only in production
-      sameSite: 'lax',  // Ensure compatibility across browsers
+      secure: false,  // Since your app is running on HTTP, set this to false
+      sameSite: 'lax',  // You can adjust this as needed
       maxAge: -1,  // Invalidate the cookie immediately
-      path: '/',  // Ensure the cookie is cleared across the entire site
+      path: '/',  // Ensure this matches the original cookie path
     }));
     res.status(200).json({ message: 'Logout successful' });
   } else {
